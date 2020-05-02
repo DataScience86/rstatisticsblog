e643c32394e902736f8c21777f6872e880110b96
datasciencebeginner <datasciencebeginners@gmail.com>
2020-05-02 07:16:10 +0530

    Updated public folder to fix iframe issue with intereactive charts article

--- public/en.search.min.5dc6e0b87f9767d178b61fba7e4fcd2e3a48f2f9cd702768b8c079c2b74aec73.js
+++ public/en.search.min.5dc6e0b87f9767d178b61fba7e4fcd2e3a48f2f9cd702768b8c079c2b74aec73.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.12b5cb6d4c3ffbc927cecbf3ad04aab7e26ae3fd6e75d9c0a6851c56f2154da4.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.4cb8ee2b642686ddb51fbc54af58030722e1c215c124274dc90fd0cc3d98cace.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
